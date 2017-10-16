3aa79a8be9e66e5de988b4d873802a0eaeccd369
Kambiz Ghoorchian <kambiz.ghoorchian@gmail.com>
2017-10-16 17:15:11 +0200

    Update extra.scala

--- scripts/extra.scala
+++ scripts/extra.scala
@@ -226,7 +226,7 @@ val from = dateparser.parse(dateFrom).getTime
 val to = dateparser.parse(dateTo).getTime
 val stats = sc.textFile(stats).map(x => (dateparser.parse(x.split(",")(2)).getTime, x))
 val sample_stats = stats.filter(x => x._1 > from && x._1 < to)
-sep.map(_._2).saveAsTextFile("sample_data")
+sample_stats.map(_._2).saveAsTextFile("sample_data")
 val sample_ids = sample_stats.map(_.split(",")(1)).map(x => (x, 1)).reduceByKey(_+_)
 val mf = sc.textFile(mutualFriends).map(_.split(",")).map(x => (x(0), x(1)))
 val temp = mf.join(sample_ids).map(x => (x._1, x._2._1))
