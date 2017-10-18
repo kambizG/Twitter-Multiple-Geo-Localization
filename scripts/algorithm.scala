d77fd524e7c25206ef571950e3bc812a68ff8cfd
Kambiz Ghoorchian <kambiz.ghoorchian@gmail.com>
2017-10-18 10:03:55 +0200

    Update algorithm.scala

--- scripts/algorithm.scala
+++ scripts/algorithm.scala
@@ -1,11 +1,11 @@
 :load /home/kambiz/data/tw_data_all_clean/tw_loi/scripts/extra.scala
 // SN
-extract_UMLP("stats.txt", "partitions_100.txt", "UMLP", 0)
+extract_UMLP("stats.txt", "partitions.txt", "mf.txt", "UMLP", 0)
 extract_CDF_UMLP("UMLP.txt", "CDF_UMLP")
 // SN + Time
-extract_UDTMLP("stats.txt", "partitions.txt", "UDTMLP", 0, "N")
-extract_UDTMLP("stats.txt", "partitions.txt", "UDTMLP", 0, "3H")
-extract_UDTMLP("stats.txt", "partitions.txt", "UDTMLP", 0, "1H")
+extract_UDTMLP("stats.txt", "partitions.txt", "mf.txt", "UDTMLP", 0, "N")
+extract_UDTMLP("stats.txt", "partitions.txt", "mf.txt", "UDTMLP", 0, "3H")
+extract_UDTMLP("stats.txt", "partitions.txt", "mf.txt", "UDTMLP", 0, "1H")
 extract_CDF_UDTMLP("UDTMLP.txt", "CDF_UDTMLP")
 
 //#################################################################################################
