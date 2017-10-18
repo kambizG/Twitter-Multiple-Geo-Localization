6d48060b8ed3ab8844149a5ff213d648522333d1
Kambiz Ghoorchian <kambiz.ghoorchian@gmail.com>
2017-10-18 11:14:24 +0200

    Update algorithm.scala

--- scripts/algorithm.scala
+++ scripts/algorithm.scala
@@ -1,12 +1,13 @@
 :load /home/kambiz/data/tw_data_all_clean/tw_loi/scripts/extra.scala
 // SN
-extract_UMLPD("stats.txt", "partitions.txt", "mf.txt", "UMLPD")
-extract_CDF_UMLPD("UMLPD.txt", "CDF_UMLPD", 100, 5)
+UMLPDC
+extract_UMLPDC("stats.txt", "partitions.txt", "mf.txt", "UMLPDC")
+extract_CDF_UMLPDC("UMLPDC.txt", "CDF_UMLPDC", 100, 5)
 // SN + Time
-extract_UDTMLPD("stats.txt", "partitions.txt", "mf.txt", "UDTMLPD", "N")
-extract_UDTMLPD("stats.txt", "partitions.txt", "mf.txt", "UDTMLPD", "3H")
-extract_UDTMLPD("stats.txt", "partitions.txt", "mf.txt", "UDTMLPD", "1H")
-extract_CDF_UDTMLPD("UDTMLPD.txt", "CDF_UDTMLPD", 100, 5)
+extract_UDTMLPDC("stats.txt", "partitions.txt", "mf.txt", "UDTMLPDC", "N")
+extract_UDTMLPDC("stats.txt", "partitions.txt", "mf.txt", "UDTMLPDC", "3H")
+extract_UDTMLPDC("stats.txt", "partitions.txt", "mf.txt", "UDTMLPDC", "1H")
+extract_CDF_UDTMLPDC("UDTMLPDC.txt", "CDF_UDTMLPDC", 100, 5)
 
 //#################################################################################################
 // Social graph + Text
