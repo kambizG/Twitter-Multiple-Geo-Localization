43bff2ccd9f01da7c111bbc97c90785c49816d5d
Kambiz <kambiz.ghoorchian@gmail.com>
2017-10-26 11:28:12 +0200

    Major update, UTMLPDC changed

--- scripts/feature_extractors/UTMLPDC.scala
+++ scripts/feature_extractors/UTMLPDC.scala
@@ -3,15 +3,16 @@
 // Topics are extracted before using a topic modeling algorithm like LDA
 // Topics are in a file containing "Statusid	TopicID" named as sid_topic.txt
 //######################################################################################
-def extract_UTMLPDC(stats: String, topics: String , partitions: String, output: String, min_count: Int) ={
+def extract_UTMLPDC(stats: String, topics: String , partitions: String, mutual_friends: String, output: String, min_count: Int) ={
 val SUL = sc.textFile(stats).map(_.split(",",7)).map(x => (x(0), (x(1), (x(4).toDouble, x(3).toDouble))))
 val ST = sc.textFile(topics).map(_.split("\t")).map(x => (x(0), x(1)))
-val ULT = SUL.join(ST).map({case(s,((u,loc),top)) => (u, (loc, top))})
-val valid_users = ULT.map(x => (x._1, 1)).reduceByKey(_+_).filter(_._2 > min_count)
-val UTML = ULT.join(valid_users).map({case(u,((loc,top),x)) => ((u, top), loc)}).groupByKey().map({case((u, top),locList) => (u,(top, geometric_median(locList.toList)))})
-val UP = sc.textFile(partitions).filter(x => !x.startsWith("#")).zipWithIndex().map(x => (x._2, x._1)).flatMapValues(x => x.split("\\s")).filter(x => !x._2.contains("-")).map(x => (x._2, x._1))
-val UTMLP = UTML.join(UP)
-UTMLP.map({case(u, ((top, (lat, lon)), p)) => u + "," + top + "," + lat + "," + lon + "," + p}).saveAsTextFile(output)
+val UTL = SUL.join(ST).map({case(s,((u,loc), top)) => ((u, top), loc)})
+val UTML = UTL.groupByKey().map({case((u, top), locList) => (u,(top, geometric_median(locList.toList)))})
+val UP = sc.textFile(partitions).map(_.split(",")).map(x => (x(0),x(1)))
+val UD = sc.textFile(mutual_friends).map(x => (x.split(",")(0), 1)).reduceByKey(_+_)
+val UMC = UTL.map(x => (x._1._1, 1)).reduceByKey(_+_)
+val UTMLPDC = UTML.join(UP).join(UD).join(UMC)
+UTMLPDC.map({case(u, ((((top, (lat, lon)), p), deg), mc)) => u + "," + lat + "," + lon + "," + p + "," + deg + "," + mc}).saveAsTextFile("output")
 }
 
 //######################################################################################
