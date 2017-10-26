01caa12005362c811fc12c7455c964a6d53eb604
Kambiz <kambiz.ghoorchian@gmail.com>
2017-10-26 14:17:50 +0200

    minor debug.

--- scripts/feature_extractors/UTMLPDC.scala
+++ scripts/feature_extractors/UTMLPDC.scala
@@ -18,7 +18,7 @@ UTMLPDC.map({case(u, ((((top, (lat, lon)), p), deg), mc)) => u + "," + top + ","
 //######################################################################################
 //######################################################################################
 def extract_CDF_UTMLPDC(in: String, res: String, minDeg: Int = 0, maxDeg: Int = Int.MaxValue, minMsgCnt: Int = 0, maxMsgCnt: Int = Int.MaxValue) = {
-val UTMLPDC = sc.textFile(in).map(_.split(",")).map(x => (x(0), (x(1), (x(2).toDouble, x(3).toDouble), x(4), x(5), x(5))))
+val UTMLPDC = sc.textFile(in).map(_.split(",")).map(x => (x(0), (x(1), (x(2).toDouble, x(3).toDouble), x(4), x(5).toInt, x(6).toInt)))
 var ML_filt_deg_cnt = UTMLPDC.filter({case(u, (t, ml, p, deg, cnt)) => deg > minDeg && deg < maxDeg && cnt > minMsgCnt && cnt < maxMsgCnt})
 val split = ML_filt_deg_cnt.map({case(u,(top, ml,p)) => (p,u)}).groupByKey().filter(_._2.size > 4).map({case(p,u) => (p, u.splitAt((u.size * 0.2).toInt))})
 val train = split.map({case(p,(tr,ts)) => (tr)}).flatMap(x => x).map(x => (x,1)).reduceByKey(_+_)
