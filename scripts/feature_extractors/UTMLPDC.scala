8cc0bc2a6f9e63fb2dfcec99c6af39b316457bec
Kambiz Ghoorchian <kambiz.ghoorchian@gmail.com>
2017-10-26 11:11:05 +0200

    Update UTMLPDC.scala

--- scripts/feature_extractors/UTMLPDC.scala
+++ scripts/feature_extractors/UTMLPDC.scala
@@ -13,3 +13,20 @@ val UP = sc.textFile(partitions).filter(x => !x.startsWith("#")).zipWithIndex().
 val UTMLP = UTML.join(UP)
 UTMLP.map({case(u, ((top, (lat, lon)), p)) => u + "," + top + "," + lat + "," + lon + "," + p}).saveAsTextFile(output)
 }
+
+//######################################################################################
+//######################################################################################
+def extract_CDF_UTMLP(in: String, res: String) = {
+val UTMLP = sc.textFile(in).map(_.split(",")).map(x => (x(0), (x(1), (x(2).toDouble,x(3).toDouble), x(4))))
+val split = UTMLP.map({case(u,(top, ml,p)) => (p,u)}).groupByKey().filter(_._2.size > 4).map({case(p,u) => (p, u.splitAt((u.size * 0.2).toInt))})
+val train = split.map({case(p,(tr,ts)) => (tr)}).flatMap(x => x).map(x => (x,1)).reduceByKey(_+_)
+val test = split.map({case(p,(tr,ts)) => (ts)}).flatMap(x => x).map(x => (x,1)).reduceByKey(_+_)
+val PTML = UTMLP.join(train).map({case(u,((top, ml, p),x)) => ((p, top), ml)}).groupByKey().map({case(ptop, mlList) => (ptop, geometric_median(mlList.toList))})
+val U_PE = UTMLP.join(test).map({case(u, ((top, ml, p),x)) => ((p, top), (u, ml))}).join(PTML).map({case(ptop, ((u, ml), pml)) => ((u, ptop), geoDistance_points(ml, pml))})
+//val AED = U_PE.map({case(u,e) => (1, (e, 1))}).reduceByKey((a,b) => (a._1 + b._1, a._2 + b._2)).map(x => (x._2._1 * 1.0)/x._2._2).collect
+//val cnt = (U_PE.count / 2.0).toInt
+//val MED = U_PE.map(_._2).sortBy(x => x).take(cnt).drop(cnt -1)
+val temp1 = U_PE.map(x => (Math.floor(x._2 * 10)/10, 1.0)).reduceByKey(_+_)
+val temp2 = sc.parallelize(Array(0.0 to 60.0 by 0.1)).flatMap(x => x).map(x => (Math.floor(x*10)/10,0.0))
+temp1.union(temp2).reduceByKey(_+_).sortBy(_._1).map(x => (x._1 + "\t" + x._2)).saveAsTextFile(res)
+}
