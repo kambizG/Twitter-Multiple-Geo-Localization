69f03a34811a5419a6e3e82e987d821cf7b25fe8
Kambiz <kambiz.ghoorchian@gmail.com>
2017-10-27 12:44:43 +0200

    new_file

---
+++ scripts/hierarchical_partitioning/extract_id_partitions.scala
@@ -0,0 +1,3 @@
+var max_comm_size = sc.textFile("max_comm_size.txt").first.toInt
+sc.textFile("tp").map(_.split("\t")).zipWithIndex().map(x => (x._2, x._1)).flatMapValues(x => x).map(x => x._2 + "," + x._1).saveAsTextFile("partitions_"+ max_comm_size)	
+exit
