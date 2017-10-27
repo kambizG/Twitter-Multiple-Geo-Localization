23f0fb8ab1ca71853c7524cdb1cc80898e5043d4
Kambiz <kambiz.ghoorchian@gmail.com>
2017-10-27 09:50:35 +0200

    Major update

--- scripts/feature_extractors/UMLPDC.scala
+++ scripts/feature_extractors/UMLPDC.scala
@@ -21,7 +21,7 @@ def extract_CDF_UMLPDC(in: String, res: String, minDeg: Int = 0, maxDeg: Int = I
 val ML = sc.textFile(in).map(_.split(",")).map(x => (x(0), ((x(1).toDouble,x(2).toDouble), x(3), x(4).toInt, x(5).toInt))).filter({case(u, (ml, p, deg, cnt)) => deg > minDeg && deg < maxDeg && cnt > minMsgCnt && cnt < maxMsgCnt})
 val UDTMLP = ML.map({case(u, (ml, p, deg, cnt)) => (u, (ml, p))})
 val UMLP = sc.textFile(in).map(_.split(",")).filter(x => x(4).toInt > minDeg && x(4).toInt < maxDeg && x(5).toInt > minMsgCnt && x(5).toInt < maxMsgCnt).map(x => (x(0), ((x(1).toDouble,x(2).toDouble), x(3))))
-val split = UMLP.map({case(u,(ml,p)) => (p,u)}).groupByKey().filter(_._2.size > 4).map({case(p,u) => (p, u.splitAt((u.size * 0.8).toInt))})
+val split = UMLP.map({case(u,(ml,p)) => (p,u)}).groupByKey().filter(_._2.size > 4).map({case(p,u) => (p, u.splitAt((u.size * 0.7).toInt))})
 val train = split.map({case(p,(tr,ts)) => (tr)}).flatMap(x => x).map(x => (x,1)).reduceByKey(_+_)
 val test = split.map({case(p,(tr,ts)) => (ts)}).flatMap(x => x).map(x => (x,1)).reduceByKey(_+_)
 val PML = UMLP.join(train).map({case(u,((ml, p),x)) => (p, ml)}).groupByKey().map({case(p, ls) => (p, geometric_median(ls.toList))})
--- scripts/feature_extractors/UTDTMLPDC.scala
+++ scripts/feature_extractors/UTDTMLPDC.scala
@@ -16,3 +16,32 @@ val UMC = SUDL.map(x => (x._2._1, 1)).reduceByKey(_+_)
 val UTDTMLPDC = UTDTML.join(UP).join(UD).join(UMC) 
 UTDTMLPDC.map({case(u, ((((top, day, time, (lat, lon)), p), deg), mc)) => u + "," + top + "," + day + "," + time  + "," + lat + "," + lon + "," + p + "," + deg + "," + mc}).saveAsTextFile(output)
 }
+
+//######################################################################################
+// Extract CDF for combination of all features including SN, Time and Topic
+//######################################################################################
+def extract_CDF_UTDTMLPDC(in: String, res: String, minDeg: Int = 0, maxDeg: Int = Int.MaxValue, minMsgCnt: Int = 0, maxMsgCnt: Int = Int.MaxValue, day: Int = -1, time: Int = -1) = {
+val ML = sc.textFile(in).map(_.split(",")).map(x => (x(0), (x(1), x(2), x(3), (x(4).toDouble, x(5).toDouble), x(6), x(7).toInt, x(8).toInt)))
+var ML_filt_deg_cnt = ML.filter({case(u, (top, d, t, ml, p, deg, cnt)) => deg > minDeg && deg < maxDeg && cnt > minMsgCnt && cnt < maxMsgCnt})
+if(day != -1)
+ if(time != -1)
+  ML_filt_deg_cnt = ML.filter({case(u, (top, d, t, ml, p, deg, cnt)) => deg > minDeg && deg < maxDeg && cnt > minMsgCnt && cnt < maxMsgCnt && d.toInt == day && t.toInt == time})
+ else
+  ML_filt_deg_cnt = ML.filter({case(u, (top, d, t, ml, p, deg, cnt)) => deg > minDeg && deg < maxDeg && cnt > minMsgCnt && cnt < maxMsgCnt && d.toInt == day})
+else if(time != -1)
+  ML_filt_deg_cnt = ML.filter({case(u, (top, d, t, ml, p, deg, cnt)) => deg > minDeg && deg < maxDeg && cnt > minMsgCnt && cnt < maxMsgCnt && t.toInt == time})
+
+val UTDTMLP = ML_filt_deg_cnt.map({case(u, (top, d, t, ml, p, deg, cnt)) => (u, (top, d, t, ml, p))})
+val split = UTDTMLP.map({case(u, (top, d, t, ml, p)) => (p, u)}).groupByKey().map(x => (x._1, x._2.toList.distinct)).filter(_._2.size > 4).map({case(p, ul) => (p, ul.splitAt((ul.size * 0.7).toInt))})
+val train = split.map({case(p, (tr, ts)) => (tr)}).flatMap(x => x).map(x => (x,1)).reduceByKey(_+_)
+val test = split.map({case(p, (tr, ts)) => (ts)}).flatMap(x => x).map(x => (x,1)).reduceByKey(_+_)
+val PTDTML = UTDTMLP.join(train).map({case(u, ((top, d, t, ml, p), x)) => ((p, top, d, t), ml)}).groupByKey().map({case(ptdt, ls) => (ptdt, geometric_median(ls.toList))})
+val U_PE = UTDTMLP.join(test).map({case(u, ((top, d, t, ml, p), x)) => ((p, top, d, t), (u, ml))}).join(PTDTML).map({case(ptdt, ((u, ml), pml)) => ((u, ptdt), geoDistance_points(ml, pml))})
+//val AED = U_PE.map({case(u,e) => (1, (e, 1))}).reduceByKey((a,b) => (a._1 + b._1, a._2 + b._2)).map(x => (x._2._1 * 1.0)/x._2._2).collect
+//val cnt = (U_PE.count / 2.0).toInt
+//val MED = U_PE.map(_._2).sortBy(x => x).take(cnt).drop(cnt -1)
+val temp1 = U_PE.map(x => (Math.floor(x._2 * 10)/10, 1.0)).reduceByKey(_+_)
+val temp2 = sc.parallelize(Array(0.0 to 60.0 by 0.1)).flatMap(x => x).map(x => (Math.floor(x*10)/10,0.0))
+val temp3 = temp1.union(temp2).reduceByKey(_+_).sortBy(_._1)
+temp3.map(x => (0, x)).groupByKey().map(x => CDF(x._2.toList)).flatMap(x => x).map(x => x._2).saveAsTextFile(res)
+}
