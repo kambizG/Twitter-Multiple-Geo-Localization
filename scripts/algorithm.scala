c88be7f7816d350fb835e5b9b0d8852d54a889aa
Kambiz Ghoorchian <kambiz.ghoorchian@gmail.com>
2017-11-10 09:58:38 +0100

    Update algorithm.scala
    minor changes. 
    Execution structure of the algorithm updated.

--- scripts/algorithm.scala
+++ scripts/algorithm.scala
@@ -1,7 +1,15 @@
-:load /home/kambiz/data/tw_data_all_clean/tw_loi/scripts/extra.scala
+PATH=$PATH:/home/kambiz/data/tw_data_all_clean/clustering_programs_5_2/bin:/home/kambiz/data/spark-2.2.0-bin-hadoop2.7/bin/
+
 // SN
+:load /home/kambiz/data/tw_data_all_clean/tw_loi/scripts/extra.scala
+:load /home/kambiz/data/tw_data_all_clean/tw_loi/scripts/feature_extractors/UMLPDC.scala
 extract_UMLPDC("stats.txt", "partitions.txt", "mf.txt", "UMLPDC")
-extract_CDF_UMLPDC("UMLPDC.txt", "CDF_UMLPDC", 100, 5)
+
+extract_CDF_UMLPDC: (in: String, res: String, minDeg: Int, maxDeg: Int, minMsgCnt: Int, maxMsgCnt: Int, pid: Int, minParSize: Int)Unit
+extract_CDF_UMLPDC("UMLPDC/UMLPDC_inf", "CDF/CDF_UMLPDC" , 0, 100000, 0, 100000, -1, 4)
+
+:load /home/kambiz/data/tw_data_all_clean/tw_loi/scripts/extra.scala
+:load /home/kambiz/data/tw_data_all_clean/tw_loi/scripts/feature_extractors/UMLPDC.scala
 // SN + Time
 extract_UDTMLPDC("stats.txt", "partitions.txt", "mf.txt", "UDTMLPDC", "N")
 extract_UDTMLPDC("stats.txt", "partitions.txt", "mf.txt", "UDTMLPDC", "3H")
