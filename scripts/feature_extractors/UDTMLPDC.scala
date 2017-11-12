98a9eccbe96b22d562679f21afe251c0d1a96d55
Kambiz <kambiz.ghoorchian@gmail.com>
2017-11-12 20:29:16 +0100

    minor change.

--- scripts/feature_extractors/UDTMLPDC.scala
+++ scripts/feature_extractors/UDTMLPDC.scala
@@ -45,7 +45,7 @@ else if(time != -1)
   ML_filt_deg_cnt = ML.filter({case(u, (d, t, ml, p, deg, cnt)) => deg > minDeg && deg < maxDeg && cnt > minMsgCnt && cnt < maxMsgCnt && t.toInt == time})
 
 val temp = ML_filt_deg_cnt.map({case(u, (d, t, ml, p, deg, cnt)) => (p, (d, t, ml, u))})
-val valid_partitions = extract_valid_partitions("partitions/partitions_inf.txt", "stats.txt", 5, 15, day, time, 50)
+val valid_partitions = extract_valid_partitions("partitions/partitions_inf.txt", "stats.txt", min_par_msg_cnt, max_par_msg_cnt, day, time, minParSize)
 var UDTMLP = temp.join(valid_partitions).map({case(p, ((d, t, ml, u), x)) => (u, (d, t, ml, p))})
 //var UDTMLP = ML_filt_deg_cnt.map({case(u, (d, t, ml, p, deg, cnt)) => (u, (d, t, ml, p))})
 if(pid != -1)
