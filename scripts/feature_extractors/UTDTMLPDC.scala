0611af573c156f69a5605cf13680f239119ed078
Kambiz <kambiz.ghoorchian@gmail.com>
2017-11-30 14:04:53 +0100

    Extract MED evaluation metric extended.

--- scripts/feature_extractors/UTDTMLPDC.scala
+++ scripts/feature_extractors/UTDTMLPDC.scala
@@ -56,3 +56,34 @@ val temp2 = sc.parallelize(Array(0.0 to 60.0 by 0.1)).flatMap(x => x).map(x => (
 val temp3 = temp1.union(temp2).reduceByKey(_+_).sortBy(_._1)
 temp3.map(x => (0, x)).groupByKey().map(x => CDF(x._2.toList)).flatMap(x => x).map(x => x._2).saveAsTextFile(res)
 }
+
+//######################################################################################
+// Extract and returns MED for combination of all features
+//######################################################################################
+def extract_MED_UTDTMLPDC(in: String, minDeg: Int = 0, maxDeg: Int = Int.MaxValue, minMsgCnt: Int = 0, maxMsgCnt: Int = Int.MaxValue, day: Int = -1, time: Int = -1, pid: Int, minParSize: Int): (Double, Double) = {
+val ML = sc.textFile(in).map(_.split(",")).map(x => (x(0), (x(1), x(2), x(3), (x(4).toDouble, x(5).toDouble), x(6), x(7).toInt, x(8).toInt)))
+var ML_filt_deg_cnt = ML.filter({case(u, (top, d, t, ml, p, deg, cnt)) => deg > minDeg && deg < maxDeg && cnt > minMsgCnt && cnt < maxMsgCnt})
+if(day != -1)
+ if(time != -1)
+  ML_filt_deg_cnt = ML.filter({case(u, (top, d, t, ml, p, deg, cnt)) => deg > minDeg && deg < maxDeg && cnt > minMsgCnt && cnt < maxMsgCnt && d.toInt == day && t.toInt == time})
+ else
+  ML_filt_deg_cnt = ML.filter({case(u, (top, d, t, ml, p, deg, cnt)) => deg > minDeg && deg < maxDeg && cnt > minMsgCnt && cnt < maxMsgCnt && d.toInt == day})
+else if(time != -1)
+  ML_filt_deg_cnt = ML.filter({case(u, (top, d, t, ml, p, deg, cnt)) => deg > minDeg && deg < maxDeg && cnt > minMsgCnt && cnt < maxMsgCnt && t.toInt == time})
+
+var UTDTMLP = ML_filt_deg_cnt.map({case(u, (top, d, t, ml, p, deg, cnt)) => (u, (top, d, t, ml, p))})
+if(pid != -1)
+ UTDTMLP = ML_filt_deg_cnt.map({case(u, (top, d, t, ml, p, deg, cnt)) => (u, (top, d, t, ml, p))}).filter({case (u, (top, d, t, ml, p)) => p.toInt == pid})
+
+val split = UTDTMLP.map({case(u, (top, d, t, ml, p)) => (p, u)}).groupByKey().map(x => (x._1, x._2.toList.distinct)).filter(_._2.size > minParSize).map({case(p, ul) => (p, ul.splitAt((ul.size * 0.7).toInt))})
+val train = split.map({case(p, (tr, ts)) => (tr)}).flatMap(x => x).map(x => (x,1)).reduceByKey(_+_)
+val test = split.map({case(p, (tr, ts)) => (ts)}).flatMap(x => x).map(x => (x,1)).reduceByKey(_+_)
+val PTDTML = UTDTMLP.join(train).map({case(u, ((top, d, t, ml, p), x)) => ((p, top, d, t), ml)}).groupByKey().map({case(ptdt, ls) => (ptdt, geometric_median(ls.toList))})
+val U_PE = UTDTMLP.join(test).map({case(u, ((top, d, t, ml, p), x)) => ((p, top, d, t), (u, ml))}).join(PTDTML).map({case(ptdt, ((u, ml), pml)) => ((u, ptdt), geoDistance_points(ml, pml))})
+val cnt = (U_PE.count / 2.0).toInt
+val MED = U_PE.map(_._2).sortBy(x => x).take(cnt).drop(cnt -1)(0).toDouble
+val Recall = (U_PE.count * 1.0/UTDTMLP.join(test).count).toDouble
+return (MED, Recall)
+}
+
+
